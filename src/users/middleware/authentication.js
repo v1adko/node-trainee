ff5cee302e87e482e0e6906e17dc892f461221e2
Burdin Bogdan <burdinbogdan@gmail.com>
2017-10-26 12:44:32 +0300

    do dao get

--- src/users/middleware/authentication.js
+++ src/users/middleware/authentication.js
@@ -41,6 +41,9 @@ module.exports.login = (req, res) => {
   }
 
   passport.authenticate('local', (err, user, info) => {
+
+    consoel.log('====login:', err, user, info);
+    
     let token;
 
     if (err) {
