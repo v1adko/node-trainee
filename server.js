231a18840ffd6d4933669618765387c9d213b98e
Burdin Bogdan <burdinbogdan@gmail.com>
2017-10-30 10:53:41 +0200

    Added some tests

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
-<<<<<<< d76684fbb68da31279889cfa465147047b2e9006
 const http = require('http');
 const app = require('./src/app');
 const config = require('./src/config/configuration');
-=======
-let http = require('http');
-let config = require('./src/config/configuration');
-let port = process.env.PORT || config.PORT || 8080;
->>>>>>> done dao get
 
 const port = process.env.PORT || config.PORT || 8080;
 
