7f2195e4d79a486d69aa940582951ceb9592ff9e
Burdin Bogdan <burdinbogdan@gmail.com>
2017-11-07 17:38:58 +0200

    fix refactoring

--- src/geolocation/services/index.js
+++ src/geolocation/services/index.js
@@ -8,23 +8,20 @@ function mapCoordinations(responsResult) {
   return responsResult.map(item => ({ lat: item.latitude, lon: item.longitude }));
 }
 
-const addressToCoordinations = (address) => {
-  geocoder
-    .geocode(address)
-    .then((result) => {
-      if (result.length === 0) {
-        throw new Error("Dos't have any matches");
-      } else { return mapCoordinations(result); }
-    })
-    .catch((err) => { throw err; });
-};
+const addressToCoordinations = address => geocoder
+  .geocode(address)
+  .then((result) => {
+    if (result.length === 0) {
+      throw new Error("Dos't have any matches");
+    } else { return mapCoordinations(result); }
+  })
+  .catch((err) => { throw err; });
 
-const coordinationsToAddress = (lat, lon) => {
+const coordinationsToAddress = (lat, lon) =>
   geocoder
     .reverse({ lat, lon })
     .then(result => ({ address: result[0].formattedAddress }))
     .catch((err) => { throw err; });
-};
 
 
 module.exports = {
