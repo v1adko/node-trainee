48b63298e16f6d23eda3ad9d6bc0fbb84c1a8353
Burdin Bogdan <burdinbogdan@gmail.com>
2017-11-07 17:30:30 +0200

    Do refactoring

--- src/geolocation/services/index.js
+++ src/geolocation/services/index.js
@@ -5,36 +5,26 @@ const options = require('../config');
 const geocoder = NodeGeocoder(options);
 
 function mapCoordinations(responsResult) {
-  return responsResult.map(item => ({
-    lat: item.latitude,
-    lon: item.longitude
-  }));
+  return responsResult.map(item => ({ lat: item.latitude, lon: item.longitude }));
 }
 
-const addressToCoordinations = address => geocoder
-  .geocode(address)
-  .then((result) => {
-    if (result.length === 0) {
-      throw new Error("Dos't have any matches");
-    } else {
-      return mapCoordinations(result);
-    }
-  })
-  .catch((err) => {
-    throw err;
-  });
+const addressToCoordinations = (address) => {
+  geocoder
+    .geocode(address)
+    .then((result) => {
+      if (result.length === 0) {
+        throw new Error("Dos't have any matches");
+      } else { return mapCoordinations(result); }
+    })
+    .catch((err) => { throw err; });
+};
 
-const coordinationsToAddress = (lat, lon) => geocoder
-  .reverse({
-    lat,
-    lon
-  })
-  .then(result => ({
-    address: result[0].formattedAddress
-  }))
-  .catch((err) => {
-    throw err;
-  });
+const coordinationsToAddress = (lat, lon) => {
+  geocoder
+    .reverse({ lat, lon })
+    .then(result => ({ address: result[0].formattedAddress }))
+    .catch((err) => { throw err; });
+};
 
 
 module.exports = {
