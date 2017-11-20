f1a45648baae3bb13e7867badcee8a996cc4e8ea
Burdin Bogdan <burdinbogdan@gmail.com>
2017-11-20 17:43:50 +0200

    Fix issues

--- src/users/controllers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as userController } from './userController';
-export {
-  default as authenticationController
-} from './authenticationController';
-export { default as userProfileController } from './userProfileController';
