10b49f115ae4701a83f46ed171f2524fae620029
Burdin Bogdan <burdinbogdan@gmail.com>
2017-11-21 12:10:12 +0200

    Fix tests

--- src/v1/events/__tests__/routers.js
+++ src/v1/events/__tests__/routers.js
@@ -4,6 +4,6 @@ jest.setTimeout(10000);
 
 describe('Test the "v1/events/" path for setting coordinates', () => {
   it('should return new event with coordinates', async () => {
-    await simulate.post('v1/events', 200, { address: 'Чичибабина 1' });
+    await simulate.post('/v1/events', 200, { address: 'Чичибабина 1' });
   });
 });
