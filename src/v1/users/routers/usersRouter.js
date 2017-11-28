4a4fc937b4848479ac252ce8db42f7feb55c74c6
Burdin Bogdan <burdinbogdan@gmail.com>
2017-11-28 15:42:54 +0200

    Do binding in generic controller

--- src/v1/users/routers/usersRouter.js
+++ src/v1/users/routers/usersRouter.js
@@ -5,28 +5,42 @@ import permissions from '../../../constants/permissions';
 
 const router = new Router();
 
-router.get('/', genericController(permissions.USER, userController.readAll));
+router.get(
+  '/',
+  genericController(permissions.USER, userController, userController.readAll)
+);
 
-router.post('/', genericController(permissions.ADMIN, userController.create));
+router.post(
+  '/',
+  genericController(permissions.ADMIN, userController, userController.create)
+);
 
 router.get(
   '/:id',
-  genericController(permissions.USER, userController.readById)
+  genericController(permissions.USER, userController, userController.readById)
 );
 
 router.put(
   '/:id',
-  genericController(permissions.ADMIN, userController.updateById)
+  genericController(
+    permissions.ADMIN,
+    userController,
+    userController.updateById
+  )
 );
 
 router.delete(
   '/:id',
-  genericController(permissions.ADMIN, userController.deleteById)
+  genericController(
+    permissions.ADMIN,
+    userController,
+    userController.deleteById
+  )
 );
 
 router.get(
   '/get/:name',
-  genericController(permissions.USER, userController.readByName)
+  genericController(permissions.USER, userController, userController.readByName)
 );
 
 export default router;
