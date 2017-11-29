caf8f9d642c0357ae489b1abc9b5c2fb8c305ba4
Burdin Bogdan <burdinbogdan@gmail.com>
2017-11-29 19:19:20 +0200

    Applying custom router for all routes

---
+++ src/errors/index.js
@@ -0,0 +1,5 @@
+export { default as TokenValidationError } from './tokenValidationError';
+export { default as PermissionsError } from './permissionsError';
+export {
+  default as EmptyAuthenticationField
+} from './emptyAuthenticationField';
