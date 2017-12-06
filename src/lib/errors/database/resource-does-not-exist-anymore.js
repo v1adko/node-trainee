5c6e7822867267a125b23c3580b12103a30c4289
Burdin Bogdan <burdinbogdan@gmail.com>
2017-12-06 19:14:16 +0200

    Rewrite messages of errors

--- src/lib/errors/database/resource-does-not-exist-anymore.js
+++ src/lib/errors/database/resource-does-not-exist-anymore.js
@@ -3,7 +3,7 @@ import BaseHttpError from '../base-http-error';
 
 class ResourceDoesNotExistAnymore extends BaseHttpError {
   constructor(message = ResourceDoesNotExistAnymore.message) {
-    const defaultMessage = 'Resource has already been deleted';
+    const defaultMessage = 'Sorry, resource has already been deleted';
     const defaultCode = HttpStatus.GONE;
 
     super(message || defaultMessage, defaultCode);
