50c43c3a6003c044bfbb876e0fb573e7498c2d1d
Burdin Bogdan <burdinbogdan@gmail.com>
2017-12-07 13:20:07 +0200

    Fix issues

--- src/middlewares/error-logger.js
+++ src/middlewares/error-logger.js
@@ -1,14 +1,15 @@
 import logger from '../lib/logger';
-import { RequestValidationError } from '../lib/errors';
+import BaseHttpError from '../lib/errors/base-http-error';
 
 // NOTE: It's only stub for logger
 
 // TODO: Should write non custom error in log file.
 //       After that developer use information from log file for writing new errors class.
 const errorLogger = (error, request, response, next) => {
-  if (error.name === RequestValidationError.name) {
+  if (error instanceof BaseHttpError) {
     return next();
   }
+
   logger.error(error);
   return next();
 };
--- src/middlewares/error-sender.js
+++ src/middlewares/error-sender.js
@@ -1,13 +1,22 @@
 import BaseHttpError from '../lib/errors/base-http-error';
 
+const getErrorCode = error => error.status || error.code || 500;
+
+const getCommonErrorResponseObject = error => ({
+  name: error.name,
+  message: `${Error.name}(${getErrorCode(error)}): ${error.message}`,
+  code: getErrorCode(error)
+});
+
 const errorSender = (error, requset, response, next) => {
   if (error instanceof BaseHttpError) {
     error.send(response);
-    next();
   } else {
-    response.status(error.status || 500).json({ message: error.message });
-    next(error);
+    response
+      .status(getErrorCode(error))
+      .json({ error: getCommonErrorResponseObject(error) });
   }
+  next(error);
 };
 
 export default errorSender;
