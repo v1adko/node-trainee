02c66e06fa0d93d7eeca13e8a04b6ba68bb28937
Burdin Bogdan <burdinbogdan@gmail.com>
2017-12-08 12:28:45 +0200

    Fix getById

--- src/v1/users/controllers/user-controller/index.js
+++ src/v1/users/controllers/user-controller/index.js
@@ -5,7 +5,6 @@ import { modelService } from '../../services/';
 import permissions from '../../../../constants/permissions';
 import permissionValidation from '../../../../lib/decorators/permission-validation-decorator';
 import requestValidator from '../../../../lib/decorators/request-validation-decorator';
-import { NotFoundError } from '../../../../lib/errors';
 import {
   tokenOnlyShema,
   createSchema,
@@ -65,10 +64,6 @@ class UserController {
   async updateById(request, response) {
     const { username, password, role } = request.data;
     const user = await this.DAO.getById(request.data.id);
-
-    if (!user) {
-      throw new NotFoundError("User doesn't exist.");
-    }
 
     if (username) {
       user.username = username;
